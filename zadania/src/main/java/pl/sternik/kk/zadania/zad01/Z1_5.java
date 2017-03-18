18b95546318d64e0a68e08f2598378eda607bea3
Krzysztof Koziol <kkoziol@gmail.com>
2017-03-18 15:07:13 +0100

    Zadanie 5 testy

---
+++ zadania/src/main/java/pl/sternik/kk/zadania/zad01/Z1_5.java
@@ -0,0 +1,49 @@
+package pl.sternik.kk.zadania.zad01;
+
+public class Z1_5 {
+
+    public static void main(String[] args) {
+        String[] tablica = { "Raz", "Dwa", "Trzy", "Cztery" };
+
+        String tmp = zrobPetleFor(tablica);
+        System.out.println(tmp);
+
+        tmp = zrobPetleForEach(tablica);
+        System.out.println(tmp);
+
+        tmp = zrobPetleWhile(tablica);
+        System.out.println(tmp);
+        
+    }
+
+	static String zrobPetleFor(String[] tablica) {
+		int i;
+		StringBuilder sb = new StringBuilder();
+		sb.append("Petla FOR");
+        for (i = 0; i <= tablica.length - 1; i++) {
+            sb.append("\n"); 
+            sb.append(tablica[i]);
+        }
+        return sb.toString();
+	}
+	static String zrobPetleWhile(String[] tablica) {
+		int i;
+		StringBuilder sb = new StringBuilder();
+		sb.append("Petla While");
+        i = 0;
+        while (i <= tablica.length - 1) {
+            sb.append("\n");
+            sb.append(tablica[i]);
+            i++;
+        }     
+        return sb.toString();
+	}	
+	static String zrobPetleForEach(String[] tablica) {
+		String tmp ="";
+		tmp = tmp + "Petla FOREach";
+		for (String string : tablica) {
+            tmp = tmp +"\n" + string;
+        }
+        return tmp;
+	}
+}
---
+++ zadania/src/test/java/pl/sternik/kk/zadania/zad01/Z1_5Test.java
@@ -0,0 +1,42 @@
+package pl.sternik.kk.zadania.zad01;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+
+public class Z1_5Test {
+
+	@Test
+	public void testDummmy() throws Exception {
+		assertThat(1).isEqualTo(1);
+	}
+
+	@Test
+	public void testPetliFor() throws Exception {
+		String[] tablica = { "Raz", "Dwa", "Trzy", "Cztery" };
+		String expected = "Petla FOR\nRaz\nDwa\nTrzy\nCztery";
+
+		String actual = Z1_5.zrobPetleFor(tablica);
+
+		assertThat(actual).isEqualTo(expected);
+	}
+
+	@Test
+	public void testPetliWhile() throws Exception {
+		String[] tablica = { "Raz", "Dwa", "Trzy", "Cztery" };
+		String expected = "Petla While\nRaz\nDwa\nTrzy\nCztery";
+
+		String actual = Z1_5.zrobPetleWhile(tablica);
+
+		assertThat(actual).isEqualTo(expected);
+	}
+
+	@Test
+	public void testPetliForEach() throws Exception {
+		String[] tablica = { "Raz", "Dwa", "Trzy", "Cztery" };
+		String expected = "Petla FOREach\nRaz\nDwa\nTrzy\nCztery";
+
+		String actual = Z1_5.zrobPetleForEach(tablica);
+
+		assertThat(actual).isEqualTo(expected);
+	}
+}
