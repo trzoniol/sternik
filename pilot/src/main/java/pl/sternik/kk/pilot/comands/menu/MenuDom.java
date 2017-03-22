e20282e9ea34f988d69fc6a704ab22c16c3b311f
Krzysztof Koziol <kkoziol@gmail.com>
2017-03-22 12:01:01 +0100

    Dodanie wentylatora

--- pilot/src/main/java/pl/sternik/kk/pilot/comands/menu/MenuDom.java
+++ pilot/src/main/java/pl/sternik/kk/pilot/comands/menu/MenuDom.java
@@ -8,11 +8,16 @@ import pl.sternik.kk.pilot.comands.CommandPusty;
 import pl.sternik.kk.pilot.comands.CommandZakoncz;
 import pl.sternik.kk.pilot.comands.swiatlo.CommandSwiatloWlacz;
 import pl.sternik.kk.pilot.comands.swiatlo.CommandSwiatloWylacz;
+import pl.sternik.kk.pilot.comands.wentylator.CommandWentylatorObroty1;
+import pl.sternik.kk.pilot.comands.wentylator.CommandWentylatorObroty2;
+import pl.sternik.kk.pilot.comands.wentylator.CommandWentylatorObroty3;
+import pl.sternik.kk.pilot.comands.wentylator.CommandWentylatorWlacz;
+import pl.sternik.kk.pilot.comands.wentylator.CommandWentylatorWylacz;
 
 public class MenuDom implements MenuPilota {
 
 	private Map<String, Command> przyciski;
-	private CommandPrzelaczMenu przelaczMenu ;
+	private CommandPrzelaczMenu przelaczMenu;
 
 	public MenuDom(Map<String, Command> przyciski, CommandPrzelaczMenu pm) {
 		this.przyciski = przyciski;
@@ -27,17 +32,12 @@ public class MenuDom implements MenuPilota {
 		// przyciski.put("3", new CommandGarazOtworzDrzwi());
 		przyciski.put("4", new CommandPusty());
 		// przyciski.put("4", new CommandGarazZamknijDrzwi());
-		przyciski.put("5", new CommandPusty());
-		// przyciski.put("5", new CommandWentylatorWlacz());
-		przyciski.put("6", new CommandPusty());
-		// przyciski.put("6", new CommandWentylatorObroty1());
-		przyciski.put("7", new CommandPusty());
-		// przyciski.put("7", new CommandWentylatorObroty2());
-		przyciski.put("8", new CommandPusty());
-		// przyciski.put("8", new CommandWentylatorObroty3());
-		przyciski.put("9", new CommandPusty());
-		// przyciski.put("9", new CommandWentylatorWylacz());
-		 przyciski.put("0", przelaczMenu);
+		przyciski.put("5", new CommandWentylatorWlacz());
+		przyciski.put("6", new CommandWentylatorObroty1());
+		przyciski.put("7", new CommandWentylatorObroty2());
+		przyciski.put("8", new CommandWentylatorObroty3());
+		przyciski.put("9", new CommandWentylatorWylacz());
+		przyciski.put("0", przelaczMenu);
 		przyciski.put("Z", new CommandZakoncz());
 		System.out.println("------>Menu Dom<------");
 	}
